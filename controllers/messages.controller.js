0ed82a67b3949eb63aaad73ba897e524456d93cc
James Vongampai <jaythomasv29@gmail.com>
2022-03-29 15:24:57 -0700

    use of templating to load data from backend and render using handlebars template

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,12 @@
+const path = require('path')
+
 function getMessages (req, res) {
-  res.send('<h1>Welcome message</h1>')
+  // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'team.jpg'))
+  res.render('messages', {
+    title: 'Messages',
+    friend: 'Elon Musk'
+  })
+  
 }
 
 function postMessage(req, res) {
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const path = require('path')
 
 const friendsRouter = require('./routes/friends.router')
 const messagesRouter = require('./routes/messages.router')
@@ -14,9 +15,21 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url} ${delta}ms`)
 })
 
+// serving website with node
+// serve images, CSS files, and JavaScript files in a directory named public:
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())  // parses incoming request with JSON payloads
 
+app.set('view engine', 'hbs');
+// A directory or an array of directories for the application's views.
+app.set('views', path.join(__dirname, 'views'))
 
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Meet the team',
+    caption: 'Now hiring for our team! Apply!'
+  })
+})
 app.use('/friends', friendsRouter)
 app.use('/messages', messagesRouter)
 
